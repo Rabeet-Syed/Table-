208696a364cbb70513bcea2edfeaad14d1c932e0
SYED RABEET UR REHMAN <rabeetsyed2@gmail.com>
2019-06-14 00:53:39 +0500

    User will enter number in decimal  e.g. 4.0 , 6.0 , 5.7 etc to print its table upto 20 .

---
+++ TABLE + Inpute value from user.py
@@ -0,0 +1,9 @@
+Table_of_number =float(input('\n\nPLESAE ENTER NUMBER IN DECIMAL TO GENERATE TABLE\n\n-------->'))
+
+
+for x in range (0,20,1) :
+    print(str(Table_of_number) + ' x ' + str (x) + ' = ' + str( Table_of_number * x ) )
+
+
+
+
